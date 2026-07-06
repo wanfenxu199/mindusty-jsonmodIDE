68212318201d4e2d20893a7b3100c5c0a11635f2
wanfenxu199 <3873068502@qq.com>
2026-07-06 08:57:18 +0800

    Add files via upload

---
+++ BuildConfig.java
@@ -0,0 +1,10 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.mindustry.modgen;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+    public final static String APPLICATION_ID = "com.mindustry.modgen";
+    public final static String BUILD_TYPE = "debug";
+    public final static long VERSION_CODE = 1;
+    public final static String VERSION_NAME = "1.11";
+}
